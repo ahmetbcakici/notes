0581b5ab720091de287ecdebbeb9a2a543281a6a
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-06 08:14:52 +0300

    some changes with models

--- server/models/user/notes.js
+++ server/models/user/notes.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const NotesSchema = new Schema({
-  name: {
+  title: {
     type: String,
     required: true,
   },
--- server/models/user/user.js
+++ server/models/user/user.js
@@ -3,14 +3,6 @@ import NotesSchema from './notes';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  surname: {
-    type: String,
-    required: true,
-  },
   username: {
     type: String,
     required: true,
@@ -20,17 +12,12 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
-  phoneNumber: {
-    type: String,
-    /* required: true, should it be? */
-  },
   emailAddress: {
     type: String,
     required: true,
     unique: true,
   },
   notes: [NotesSchema],
-  /* group */
 });
 
 export default mongoose.model('user', UserSchema);
