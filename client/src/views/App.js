0980cf5fa17ee120f4a946838f4ef87b67d78893
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-07 06:28:16 +0300

    some minor changes with codebase

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -7,7 +7,7 @@ import NoteArea from '../components/NoteArea';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar/>
       <Sidebar/>
     <NoteArea/>
