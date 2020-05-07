80aed71e604428557e9fe11254b7bedaa5756aa6
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-07 06:29:04 +0300

    'exact'keyword removed temporary

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-
 import App from './views/App';
 import Login from './views/Login';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Login} exact />
-      <Route path="/app" component={App} exact />
+      <Route path='/' component={Login} />
+      <Route path='/app' component={App}  />
     </BrowserRouter>
   );
 }
