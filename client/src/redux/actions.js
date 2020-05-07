e2e9ad3604a9489b0cd98e2834c733d8c5fe69bd
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-07 05:21:13 +0300

    error handling with try/catch

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,16 +2,24 @@ import axios from 'axios';
 
 export const login = (credentials) => {
   return async (dispatch) => {
-    const {data} = await axios.post('http://localhost:3333/login', credentials);
-    localStorage.setItem('jwt', data.token);
-    dispatch({type: 'LOGIN', payload: data.doc});
+    try {
+      const {data} = await axios.post(
+        'http://localhost:3333/login',
+        credentials
+      );
+      localStorage.setItem('jwt', data.token);
+      dispatch({type: 'LOGIN', payload: data.doc});
+    } catch ({response}) {
+      const {data/*, status */} = response;
+      dispatch({type: 'LOGIN', payload: data});
+    }
   };
 };
 
 export const auth = (token) => {
   return async (dispatch) => {
     const {data} = await axios.post('http://localhost:3333/auth', {token});
-    
+    console.log("auth func from act")
     dispatch({type: 'AUTH', payload: data.user});
   };
 };
