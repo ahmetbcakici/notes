d5ef0332658916f6acb1a6eea9367fd22af683d2
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-08 07:59:08 +0300

    updateNote operation moved to redux

--- client/src/components/NoteArea.js
+++ client/src/components/NoteArea.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {connect} from 'react-redux';
 
-function NoteArea({selectedNote}) {
+import {updateNote} from '../redux/actions/note';
+
+function NoteArea({selectedNote, updateNote, user}) {
+  const [noteContent, setNoteContent] = useState('');
+
+  useEffect(() => {
+    setNoteContent(
+      selectedNote.content === undefined ? '' : selectedNote.content
+    );
+  }, [selectedNote]);
+
   return (
     <div>
       <p>{selectedNote.title}</p>
-      <textarea cols='30' rows='10'>{selectedNote.content}</textarea>
+      <textarea
+        cols="30"
+        rows="10"
+        value={noteContent}
+        onChange={({target}) => setNoteContent(target.value)}
+      ></textarea>
+      <button
+        onClick={() =>
+          updateNote(
+            selectedNote._id,
+            user._id,
+            selectedNote.title,
+            noteContent
+          )
+        }
+      >
+        SAVE
+      </button>
     </div>
   );
 }
 
-const mapStateToProps = state => ({
-  selectedNote:state.selectedNoteReducer
-})
+const mapStateToProps = (state) => ({
+  selectedNote: state.selectedNoteReducer,
+  user: state.userReducer,
+});
 
-export default connect(mapStateToProps)(NoteArea);
+const mapDispatchToProps = {
+  updateNote,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NoteArea);
