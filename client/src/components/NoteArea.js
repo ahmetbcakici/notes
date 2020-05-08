83301b990c15e9083bd90ba08fe8c0b21cf4bb91
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-08 11:17:27 +0300

    auto-save (debounce) is working

--- client/src/components/NoteArea.js
+++ client/src/components/NoteArea.js
@@ -2,37 +2,65 @@ import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 
 import {updateNote} from '../redux/actions/note';
+import '../style/NoteArea.css';
+
+const resetTimeout = (id, newID) => {
+  clearTimeout(id);
+  return newID;
+};
 
 function NoteArea({selectedNote, updateNote, user}) {
   const [noteContent, setNoteContent] = useState('');
+  const [timeout, setTTimeout] = useState(null);
+  const [saved, setSaved] = useState(false);
 
   useEffect(() => {
     setNoteContent(
-      selectedNote.content === undefined ? '' : selectedNote.content
+      selectedNote.content !== undefined ? selectedNote.content : '' 
     );
   }, [selectedNote]);
 
+  const editValue = (noteContent) => {
+    setTTimeout(resetTimeout(timeout, setTimeout(saveValue, 2400)));
+    setNoteContent(noteContent);
+  };
+
+  const saveValue = () => {
+    setSaved(true);
+    updateNote(
+      selectedNote._id,
+      user._id,
+      selectedNote.title,
+      noteContent
+    )
+    setTimeout(() => setSaved(false), 1000);
+  };
+
   return (
     <div>
-      <p>{selectedNote.title}</p>
-      <textarea
-        cols="30"
-        rows="10"
-        value={noteContent}
-        onChange={({target}) => setNoteContent(target.value)}
-      ></textarea>
-      <button
-        onClick={() =>
-          updateNote(
-            selectedNote._id,
-            user._id,
-            selectedNote.title,
-            noteContent
-          )
-        }
-      >
-        SAVE
-      </button>
+      <div id="wrapper">
+        <div id="margin">
+          Title: <input id="title" type="text" value={selectedNote.title} readOnly/>
+        </div>
+        <textarea
+          placeholder="Enter something funny."
+          id="text"
+          name="text"
+          value={noteContent}
+          onChange={(e) => editValue(e.currentTarget.value)}
+          style={{
+            overflow: 'hidden',
+            wordWrap: 'break-word',
+            resize: 'none',
+            height: '360px',
+          }}
+        ></textarea>
+        <div style={{display: saved ? '' : 'none'}}>
+          <p>Saved Successfully</p>
+        </div>
+
+        <br />
+      </div>
     </div>
   );
 }
