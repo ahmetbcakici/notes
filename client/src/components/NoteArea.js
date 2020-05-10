2c8394e2854d30db73db5b8d727c6d701b155793
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-10 19:16:02 +0300

    toastr component imported

--- client/src/components/NoteArea.js
+++ client/src/components/NoteArea.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 
 import {updateNote} from '../redux/actions/note';
+import Toastr from './Toastr';
 import '../style/NoteArea.css';
 
 const resetTimeout = (id, newID) => {
@@ -11,49 +12,51 @@ const resetTimeout = (id, newID) => {
 
 function NoteArea({selectedNote, updateNote, user}) {
   const [noteContent, setNoteContent] = useState('');
+  const [showToastr, setShowToastr] = useState('');
   const [timeout, setTTimeout] = useState(null);
   const [saved, setSaved] = useState(false);
 
   useEffect(() => {
     setNoteContent(
-      selectedNote.content !== undefined ? selectedNote.content : '' 
+      selectedNote.content !== undefined ? selectedNote.content : ''
     );
   }, [selectedNote]);
 
   const editValue = (noteContent) => {
     setNoteContent(noteContent);
-    setTTimeout(resetTimeout(timeout, setTimeout(() => saveValue(noteContent), 1000)));
+    setTTimeout(
+      resetTimeout(
+        timeout,
+        setTimeout(() => saveValue(noteContent), 1000)
+      )
+    );
   };
 
   const saveValue = (noteContent) => {
     setSaved(true);
-    updateNote(
-      selectedNote._id,
-      user._id,
-      selectedNote.title,
-      noteContent
-    )
-    setTimeout(() => setSaved(false), 1000);
+    setShowToastr(true);
+    updateNote(selectedNote._id, user._id, null, noteContent);
+    setTimeout(() => {
+      setSaved(false);
+      setShowToastr(false);
+    }, 1000);
   };
 
   return (
     <div>
-      <div id="wrapper">
-        {/* <div id="margin">
-          Title: <input id="title" type="text" value={selectedNote.title} readOnly/>
-        </div> */}
-        <textarea
-          placeholder="Enter something funny."
-          id="text"
-          /* value={`${selectedNote.title}\n\n${noteContent}`} */
-          value={noteContent}
-          onChange={(e) => editValue(e.currentTarget.value)}
-        ></textarea>
-        <br/>
-        <div style={{display: saved ? '' : 'none',color:'white'}}>
-          <p>Saved Successfully</p>
-        </div>
-      </div>
+      <Toastr
+        title="Auto-Saving"
+        color="info"
+        position="top-right"
+        visibility={showToastr}
+      ></Toastr>
+      <textarea
+        id="text"
+        /* value={`${selectedNote.title}\n\n${noteContent}`} */
+        value={noteContent}
+        onChange={(e) => editValue(e.currentTarget.value)}
+      ></textarea>
+      <br />
     </div>
   );
 }
---
+++ client/src/components/Toastr.js
@@ -0,0 +1,68 @@
+import React, {useMemo, useState} from 'react';
+import cx from 'classnames';
+
+import '../style/Toastr.css';
+
+const Toastr = ({
+  title,
+  message,
+  children,
+  visibility,
+  color = 'info',
+  position = 'top-right',
+  duration = 3000,
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  const onShow = () => {
+    setVisible(true);
+
+    if (duration > 0) {
+      setTimeout(() => {
+        setVisible(false);
+      }, duration);
+    }
+  };
+
+  const onHide = () => {
+    setVisible(false);
+  };
+
+  const containerClass = useMemo(
+    () =>
+      cx('toastr-container', {
+        'toastr-tr': position === 'top-right',
+        'toastr-tl': position === 'top-left',
+        'toastr-tc': position === 'top-center',
+        'toastr-br': position === 'bottom-right',
+        'toastr-bl': position === 'bottom-left',
+        'toastr-bc': position === 'bottom-center',
+        'toastr-hidden': !visibility,
+      }),
+    [position, visibility]
+  );
+
+  const colorClass = useMemo(
+    () =>
+      cx('toastr-content', {
+        'toastr-info': color === 'info',
+        'toastr-success': color === 'success',
+        'toastr-warning': color === 'warning',
+        'toastr-error': color === 'error',
+      }),
+    [color]
+  );
+
+  return (
+    <React.Fragment>
+      <div className={containerClass} style={{display:visibility ? '' : 'none'}}>
+        <div className={colorClass}>
+          <div className="toast-title">{title}</div>
+          <div className="toast-message">{message}</div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Toastr;
